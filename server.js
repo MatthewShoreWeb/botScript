732429a25bc13bbdd65c74ce3648b1d8083e041d
Matthew Shore <MazzaMDS@gmail.com>
2020-05-02 22:25:33 +0100

    Server changed.

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+function onRequest (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+}
+
+http.createServer(onRequest).listen(8000);
