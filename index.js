d9b5a73d9a7ca0ae834b02d1802820581eec4cc5
Matthew Shore <MazzaMDS@gmail.com>
2020-05-02 23:20:52 +0100

    Open pages.

---
+++ index.js
@@ -0,0 +1,29 @@
+let accountNumber = 1;
+let windowCount = 0;
+
+document.querySelector('#input').addEventListener('change', function () {
+    try {
+        accountNumber = parseInt(document.querySelector('#input').value);
+        if (accountNumber > 50) {
+            accountNumber = 50;
+        }
+    } catch (e) {
+    }
+});
+
+// Recursively opens new tabs with a delay.
+function openLoop () {
+    setTimeout(function () {
+        window.open('https://www.strongholdkingdoms.com/full_site.php');
+        windowCount++;
+        if (windowCount < accountNumber) {
+            openLoop();
+        }
+    }, 1500);
+};
+
+document.querySelector('#go').addEventListener('click', function () {
+    openLoop();
+});
+
+
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-function onRequest (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
+function onRequest(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
   fs.readFile('./index.html', null, function (error, data) {
     if (error) {
       response.writeHead(404);
