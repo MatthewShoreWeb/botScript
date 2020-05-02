0638f6d245c8a7c79f7602158342fffd62b7fde3
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2020-05-02 18:57:47 +0100

    Update account.js

--- account.js
+++ account.js
@@ -4,13 +4,8 @@ function randomString (x, y) {
     return Math.random().toString(36).substr(x, y);
 }
 
-let email = function () {
-    return email = randomString(2, 5) + '@' + randomString(2, 5) + '.com';
-}
-
-let password = function () {
-    return randomString(0, 8);
-}
+let email = randomString(2, 5) + '@' + randomString(2, 5) + '.com';
+let password = randomString(0, 8);
 
 // Enter random information.
 document.querySelector('#txtEmail').getAttribute('value') = email;
