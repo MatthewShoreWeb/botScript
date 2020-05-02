698200415ed5ad6dcd2928bb646fadeb154876f5
Matthew Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2020-05-02 20:18:36 +0100

    Update account.js

--- account.js
+++ account.js
@@ -1,20 +1,27 @@
 // Email bot script.
 
-function randomString (x, y) {
-    return Math.random().toString(36).substr(x, y);
+function createAccount() {
+    function randomString(x, y) {
+        return Math.random().toString(36).substr(x, y);
+    }
+
+    let email = randomString(2, 5) + '@' + randomString(2, 5) + '.com';
+    let password = randomString(0, 8);
+
+    // Enter random information.
+    document.querySelector('#txtEmail').value = email;
+    document.querySelector('#passwordfield').value = password;
+    document.querySelector('#passwordfield2').value = password;
+
+    // Accept the terms and conditions.
+    document.querySelector('#termsCondtion').checked = true;
+
+    // Create the account.
+    document.querySelector('#btnCreate').form.submit();
+
+    // setTimeout(function () {
+    //     window.location.href = 'https://www.strongholdkingdoms.com/full_site.php';
+    // }, 500);
+
 }
-
-let email = randomString(2, 5) + '@' + randomString(2, 5) + '.com';
-let password = randomString(0, 8);
-
-// Enter random information.
-document.querySelector('#txtEmail').getAttribute('value') = email;
-document.querySelector('#passwordfield').getAttribute('value') = password;
-document.querySelector('#passwordfield2').getAttribute('value') = password;
-
-// Accept the terms and conditions.
-document.querySelector('#termsCondtion').checked = true;
-
-// Create the account.
-document.querySelector('#btnCreate').form.submit();
 
